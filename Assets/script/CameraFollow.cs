c5cc42cf6cdc853602eb1d09227a2d5beb25bb1f
KitVanDeBunt <kitlvdbunt@hotmail.com>
2013-12-06 22:55:36 +0100

    basic player movement + basic camera script

---
+++ Assets/script/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	private Transform playerTrans;
+	private Vector2 velocity;
+	public float smoothTime = 0.2f;
+
+	private void Start () {
+		playerTrans = GameObject.Find("player").GetComponent<Transform>();
+	}
+
+	private void Update(){
+		float newXPos = Mathf.SmoothDamp(transform.position.x, 
+		                                 playerTrans.position.x, 
+		                                 ref velocity.x,smoothTime);
+		float newYPos = Mathf.SmoothDamp(transform.position.y, 
+		                                 playerTrans.position.y, 
+		                                 ref velocity.y,smoothTime);
+		transform.position = new Vector3(newXPos,newYPos,transform.position.z);
+	}
+}
---
+++ Assets/script/PlayerMove.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+	public float angularVelocity;
+	public float drag = 0.97f;
+	public float dashForce = 200;
+	public float moveForce = 5;
+	public float rotateForce = 2;
+
+	private void FixedUpdate () {
+		Move();
+		Rotate();
+
+	}
+
+	private void Rotate(){
+		//get input
+		float hor = -Input.GetAxis("Horizontal");
+		//add force
+		rigidbody2D.AddTorque(hor*rotateForce);
+		//limit force
+		if(rigidbody2D.angularVelocity > angularVelocity){
+			rigidbody2D.angularVelocity = angularVelocity;
+		}else if(rigidbody2D.angularVelocity < -angularVelocity){
+			rigidbody2D.angularVelocity = -angularVelocity;
+		}
+	}
+
+	private void Move(){
+		//dash
+		bool jump = Input.GetButtonDown("Jump");
+		if(jump){
+			rigidbody2D.AddForce( ForceAndAngleToDirection(dashForce,transform.rotation.eulerAngles.z));
+		}
+		//move
+		bool upKeyDown = Input.GetKey(KeyCode.UpArrow);
+		if(upKeyDown){
+			rigidbody2D.AddForce( ForceAndAngleToDirection(moveForce,transform.rotation.eulerAngles.z));
+		}
+		//drag
+		float newX = 0;
+		float newY = 0;
+		if(rigidbody2D.velocity.x!=0){
+			newX = rigidbody2D.velocity.x*drag;
+		}else{
+			newX = rigidbody2D.velocity.x;
+		}
+		if(rigidbody2D.velocity.y!=0){
+			newY= rigidbody2D.velocity.y*drag;
+		}else{
+			newY= rigidbody2D.velocity.y;
+		}
+		rigidbody2D.velocity = new Vector2(newX,newY);
+	}
+
+	private Vector2 ForceAndAngleToDirection(float force,float angle){
+		float xForce = force * Mathf.Sin(angle*Mathf.PI/180);
+		float yForce = force * Mathf.Cos(angle*Mathf.PI/180);
+		return new Vector2(-xForce,yForce);
+	}
+}
