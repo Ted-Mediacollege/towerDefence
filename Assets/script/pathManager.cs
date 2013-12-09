1331093f715f36565909e2320e0dfe1d0b9db113
ted80-Laptop <meatkroket@hotmail.com>
2013-12-09 14:43:03 +0100

    PathManager

---
+++ Assets/script/pathManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class pathManager : MonoBehaviour 
+{
+	private GameObject[] paths;
+
+	void Start () {
+		int childscount = this.gameObject.transform.childCount;
+		paths = new GameObject[childscount];
+		
+		for(int i = 0; i < childscount; i++) {
+			Transform pathTransf = this.gameObject.transform.GetChild(i);
+			GameObject path = pathTransf.gameObject;
+			paths[i] = path;
+		}
+	}
+	
+	Vector3[] getClosestPath(Vector3 entitypos) {
+		return null;
+	}
+}
