844a932faa154c89bb91ebb273a46dea57a6c1fc
KitVanDeBunt <kitlvdbunt@hotmail.com>
2013-12-09 15:40:02 +0100

    enemy Spawning

--- Assets/script/enemyManager.cs
+++ Assets/script/enemyManager.cs
@@ -1,15 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class enemyManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	private List<GameObject> enemies;
+	public Transform[] SpawnPoints; 
+	private int timer;
+	private GameObject test;
+
+	private void Start(){
+		enemies = new List<GameObject>();
 	}
-	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		timer++;
+		int randomSpawn = Random.Range(0,3);
+		Vector3 spawn = (Vector3)SpawnPoints[randomSpawn].transform.position;
+		if(timer%50==0){
+			GameObject newEnemy = (GameObject)GameObject.Instantiate(Resources.Load("enemy"),
+			                                                         spawn,
+			                                                         Quaternion.identity);
+			enemies.Add(newEnemy);
+			//enemies[enemies.Count].GetComponent<enemy>().spawnPoints = SpawnPoints;
+		}
 	}
 }
