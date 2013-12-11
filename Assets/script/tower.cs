861b4e245164c9ae58bed6f99581627433a2659c
KitVanDeBunt <kitlvdbunt@hotmail.com>
2013-12-11 09:13:58 +0100

    tower update/manager renamed

--- Assets/script/tower.cs
+++ Assets/script/tower.cs
@@ -8,17 +8,34 @@ public class tower : MonoBehaviour {
 	public float maxAngularVelocity = 50;
 	public float maxRotForce = 90;
 	public float maxangularVelocity = 90;
+	public float agroDistance = 5;
 	void Start () {
-		enemyMngr = GameObject.Find("manager").GetComponent<enemyManager>() as enemyManager;
+		enemyMngr = GameObject.Find("enemyManager").GetComponent<enemyManager>() as enemyManager;
 		playerTrance = GameObject.Find("player").GetComponent<Transform>() as Transform;
 	}
 
+	float distCurrentTarget;
+	Vector3 currentTarget;
 	void FixedUpdate(){
-		//Debug.Log(enemyMngr.enemies.Count);
+
+		//get targer
+		if(enemyMngr.enemies.Count>0){
+			currentTarget = enemyMngr.enemies[0].transform.position;
+			distCurrentTarget = Vector3.Distance(transform.position,currentTarget);
+		}
+		for (int i = 0; i < enemyMngr.enemies.Count; i++){
+			distCurrentTarget = Vector3.Distance(transform.position,currentTarget);
+			float distNewTarget = Vector3.Distance(transform.position,enemyMngr.enemies[i].transform.position);
+			if(distNewTarget<distCurrentTarget){
+				currentTarget = enemyMngr.enemies[i].transform.position;
+			}
+		}
 		//transform.rotation =  Quaternion.Euler(new Vector3(0, 0, movement.RotateToPoint(transform,playerTrance.position)));
-		rigidbody2D.AddTorque(movement.RotateForce(transform,
-		                                           playerTrance.position,
-		                                           maxRotForce));
+		if(distCurrentTarget <agroDistance){
+			rigidbody2D.AddTorque(movement.RotateForce(transform,
+			                                           currentTarget,
+			                                           maxRotForce));
+		}
 		//limit force
 		rigidbody2D.angularVelocity = movement.limitTorque(rigidbody2D.angularVelocity,maxAngularVelocity);
 	}
