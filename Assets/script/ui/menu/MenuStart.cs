1a41d099d3bdde725dbc7c7355054bbb72d4d230
Kit van de Bunt <kitlvdbunt@hotmail.com>
2014-01-22 17:04:49 +0100

    leve2 added + menu update

--- Assets/script/ui/menu/MenuStart.cs
+++ Assets/script/ui/menu/MenuStart.cs
@@ -16,11 +16,15 @@ public class MenuStart : MonoBehaviour {
 	
 	public float native_width  = 1280;
 	public float native_height  = 720;
+	public float current_width  = 1280;
+	public float current_height  = 720;
 	public guiButton[] buttons;
 
 	public Vector3 posMenuStart = new Vector3(0,0,0);
 	public Vector3 posMenuLevel = new Vector3(1280,0,0);
 	public Vector3 posMenuCredits = new Vector3(1280,0,0);
+	private Vector3 menuPosScale;
+	private Vector3 menuPosTarget;
 
 	//transitio
 	public Vector3 levelMenuDisplaysment = new Vector3(0, 0, 0);
@@ -34,9 +38,12 @@ public class MenuStart : MonoBehaviour {
 	public GUISkin style;
 
 	void Start(){
+		current_width = Screen.width;
+		current_height = Screen.height;
 		traget = transform.position;
 		startPos = transform.position;
 		levelMenuDisplaysment = new Vector3(0, 0, 0);
+		menuPosTarget = new Vector3(0, 0, 0);
 	}
 	void Update() {
 		float distCovered = (Time.time - startTime) * speed;
@@ -50,39 +57,67 @@ public class MenuStart : MonoBehaviour {
 		startPos = levelMenuDisplaysment;
 		startTime = Time.time;
 		journeyLength = Vector3.Distance(startPos, traget);
+		
 	}
 
 	void OnGUI(){
-		GUI.skin = style;
-
 		float rx = Screen.width / native_width;
 		float ry = Screen.height / native_height;
-		GUI.matrix = Matrix4x4.TRS ( levelMenuDisplaysment,Quaternion.identity,  new Vector3 (rx, ry, 1));
-
-		GUI.Box(new Rect((1280/2)-250,(720/2)-200,500,400), "Menu");
-
-		if(GUI.Button(new Rect((1280/2)-80,(720/2)-80,160,60), "Level Menu")) {
-			switchScreen(posMenuLevel);
+		menuPosScale = new Vector3(rx,ry,0);
+		
+		// resolution change
+		if(current_width!=Screen.width||current_height!=Screen.height){
+			current_width = Screen.width;
+			current_height = Screen.height;
+			switchScreen(Vector3.Scale(menuPosTarget,menuPosScale));
 		}
-		if(GUI.Button(new Rect((1280/2)-80,(720/2)+0,160,60), "Credit Menu")) {
-			switchScreen(posMenuCredits);
+		
+		GUI.skin = style;
+
+		//start menu
+		GUI.matrix = Matrix4x4.TRS ( levelMenuDisplaysment,Quaternion.identity,  new Vector3 (rx, ry, 1));
+		//GUI.Box(new Rect((1280/2)-250,(720/2)-200,500,400), "Menu");
+		if(GUI.Button(new Rect((1280/2)-80-400,(720/2)-80,160,60), "Level Menu")) {
+			switchScreen(Vector3.Scale( posMenuLevel,menuPosScale));
+			menuPosTarget = posMenuLevel;
+		}
+		if(GUI.Button(new Rect((1280/2)-80-400,(720/2)+0,160,60), "Credit Menu")) {
+			switchScreen(Vector3.Scale( posMenuCredits,menuPosScale));
+			menuPosTarget = posMenuCredits;
 		}
 
 		// level menu
-		GUI.matrix = Matrix4x4.TRS ( levelMenuDisplaysment-posMenuLevel,Quaternion.identity,  new Vector3 (rx, ry, 1));
+		GUI.matrix = Matrix4x4.TRS ( levelMenuDisplaysment-Vector3.Scale( menuPosScale,posMenuLevel),Quaternion.identity,  new Vector3 (rx, ry, 1));
 		foreach(guiButton but in buttons){
-			if(GUI.Button(new Rect(but.xPos,but.yPos,80,20), but.text)) {
+			if(GUI.Button(new Rect(but.xPos,but.yPos,80,80), but.text)) {
 				Application.LoadLevel(but.levelName);
 			}
 		}
-		if(GUI.Button(new Rect(100,100,80,20), "Back")) {
-			switchScreen(posMenuStart);
+		if(GUI.Button(new Rect(100,600,100,60), "Back")) {
+			switchScreen(Vector3.Scale( posMenuStart,menuPosScale));
+			menuPosTarget = posMenuStart;
 		}
 		// credit menu
-		GUI.matrix = Matrix4x4.TRS ( levelMenuDisplaysment-posMenuCredits,Quaternion.identity,  new Vector3 (rx, ry, 1));
-		if(GUI.Button(new Rect(100,100,80,20), "Back")) {
-			switchScreen(posMenuStart);
+		GUI.matrix = Matrix4x4.TRS ( levelMenuDisplaysment-Vector3.Scale( menuPosScale,posMenuCredits),Quaternion.identity,  new Vector3 (rx, ry, 1));
+		if(GUI.Button(new Rect(100,600,100,60), "Back")) {
+			switchScreen(Vector3.Scale( posMenuStart,menuPosScale));
+			menuPosTarget = posMenuStart;
 		}
-
+		GUI.TextArea(new Rect(300,200,680,320)
+		             ,"R1\n"+
+		             "R2\n"+
+		             "R3\n"+
+		             "R4\n"
+		);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
